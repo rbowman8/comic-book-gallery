ac727f1ba6b682b37e9bbd76641dff67ca56312e
robert bowman <bowcorp@gmail.com>
2017-02-18 00:52:29 -0500

    init commit

---
+++ src/distanceRan/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace distanceRan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //what is your name
+            //tell the user my name   hi "user "my name is slim shady
+            //how do you feel
+            //three user response options
+            //question 1 great
+            //response 2 meh
+            //response 3 horrible
+            //three answers to user reponses
+            //response 1 
+            //response 2
+            //response 3
+            //answer off of selected answer
+            //creative from here
+            //prompt distance in miles you want to run
+            Console.Write("how many miles do you want to run in word form: ");
+            //reading in console
+            string miles = Console.ReadLine();
+            //display time/miles
+            Console.WriteLine("So you really want to run " + miles + " huh");
+            //how much more in miles do you want to run
+            //display total time at that point
+            //display total miles ran
+        }
+    }
+}
